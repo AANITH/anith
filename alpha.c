fc34ea9132f9e6da18bea18cc21c139a4537b5e7
AANITH <52598523+AANITH@users.noreply.github.com>
2019-07-23 12:06:03 +0530

    Add files via upload

---
+++ alpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if((ch>='a'&& ch<='z') || (ch>='A'&& ch<='Z'))
+    {
+    printf("alphabet");
+    }
+    else
+    {
+    printf("not");
+    }
+
+    return 0;
+}
