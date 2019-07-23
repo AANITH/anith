872ce3f575e262af34e62dc08143885c9a2f7585
AANITH <52598523+AANITH@users.noreply.github.com>
2019-07-23 13:46:58 +0530

    Add files via upload

---
+++ largest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,n3;
+    scanf("%d%d%d",&n1,&n2,&n3);
+    if(n1>n2)
+    {
+        if(n1>n3)
+        {
+        printf("n1");
+        }
+        else
+        {
+        printf("n3");
+        }
+    }
+   else if(n2>n3)
+    {
+        printf("n2");
+    }
+    
+        else
+        {
+        printf("n3");
+        }
+        
+    
+    return 0;
+}
