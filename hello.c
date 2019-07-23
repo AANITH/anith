707a40564898682d3da1a6587d9f84bb4066c1cb
AANITH <52598523+AANITH@users.noreply.github.com>
2019-07-23 14:44:18 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("hello world\n");
+    }
+   if(n==0)
+    {
+        printf(" ");
+    }
+
+    
+    return 0;
+}
+
+    
