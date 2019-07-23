2d6cf281b78452d9d1c1921092814cfdb1b1a734
AANITH <52598523+AANITH@users.noreply.github.com>
2019-07-23 11:45:01 +0530

    Add files via upload

---
+++ vow.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if(ch=='$')
+    {
+        printf("invalid");
+    }
+    else if(ch=='a'||ch=='e'|| ch=='i'||ch=='o'||ch=='u'|| ch=='A'|| ch=='E' || ch=='I'|| ch=='O'||ch=='U')
+    {
+    printf("vowel");
+    }
+    else
+    {
+    printf("consonant");
+    }
+return 0;
+}
